edb2cdf6d9e35c7899c68374541bc7c34061788e
Daniel P. Clark <6ftdan@gmail.com>
2018-11-20 00:28:07 -0500

    individualize dep checks

--- src/lib.rs
+++ src/lib.rs
@@ -8,28 +8,55 @@ pub mod asset_path;
 
 pub type Manifest = HashMap<String, String>;
 
+// namespaces provide helpful panic info
+pub mod dependency {
+    pub mod validation {
+        use std::process::Command;
+
+        pub fn yarn() -> bool {
+            Command::new("yarn").arg("-v").output()
+                .expect("Error: Yarn needs to be available.").status.success()
+        }
+
+        pub fn ruby() -> bool {
+            Command::new("ruby").arg("-v").output()
+                .expect("Error: Ruby needs to be available.").status.success()
+        }
+
+        pub fn node() -> bool {
+            Command::new("node").arg("-v").output()
+                .expect("Error: Node needs to be available.").status.success()
+        }
+
+        pub fn rake() -> bool {
+            Command::new("gem").arg("which").arg("rake").output()
+                .expect("Error: Rake needs to be available.").status.success()
+        }
+
+        pub fn bundler() -> bool {
+            Command::new("gem").arg("which").arg("bundler").output()
+                .expect("Error: Bundler needs to be available.").status.success()
+        }
+
+        pub fn webpacker_cli() -> bool {
+            Command::new("webpacker-cli").output()
+                .expect("Error: WebpackerCli needs to be available.").status.success()
+        }
+
+        pub fn webpacker_initialization() -> bool {
+            Command::new("rake").arg("webpacker:verify_install").output()
+                .expect("Error: Webpacker not verified as installed.  Did you try `webpacker-cli init` ?")
+                .status.success()
+        }
+    }
+}
+
+use dependency::validation::*;
+
 pub fn valid_project_dir() -> bool {
-    Command::new("yarn").arg("-v")
-        .output().expect("Error: Yarn needs to be available.")
-        .status.success() &&
-        Command::new("ruby").arg("-v")
-        .output().expect("Error: Ruby needs to be available.")
-        .status.success() &&
-        Command::new("node").arg("-v")
-        .output().expect("Error: Node needs to be available.")
-        .status.success() &&
-        Command::new("gem").arg("which").arg("rake")
-        .output().expect("Error: Rake needs to be available.")
-        .status.success() &&
-        Command::new("gem").arg("which").arg("bundler")
-        .output().expect("Error: Rake needs to be available.")
-        .status.success() &&
-        Command::new("webpacker-cli")
-        .output().expect("Error: WebpackerCli needs to be available.")
-        .status.success() &&
-        Command::new("rake").arg("webpacker:verify_install")
-        .output().expect("Error: Webpacker not verified as installed.  Did you try `webpacker-cli init` ?")
-        .status.success()
+    yarn() && ruby() && node() && rake() &&
+        bundler() && webpacker_cli() &&
+        webpacker_initialization()
 }
 
 pub fn compile() -> Result<(), String> {
